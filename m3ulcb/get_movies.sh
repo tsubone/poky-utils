cb26dbbc6b30eaeef17fda4c8b339efac07016de
Takashi Tsubone <takashi@hilbert>
2018-02-18 23:50:28 +0900

    update.

---
+++ m3ulcb/get_movies.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+wget http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4
+wget http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_2mb.mp4
+wget http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_5mb.mp4
+wget http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_10mb.mp4
+wget http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_20mb.mp4
+wget http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_30mb.mp4
+wget http://www.sample-videos.com/video/mp4/480/big_buck_bunny_480p_1mb.mp4
+wget http://www.sample-videos.com/video/mp4/480/big_buck_bunny_480p_2mb.mp4
+wget http://www.sample-videos.com/video/mp4/480/big_buck_bunny_480p_5mb.mp4
+wget http://www.sample-videos.com/video/mp4/480/big_buck_bunny_480p_10mb.mp4
+wget http://www.sample-videos.com/video/mp4/480/big_buck_bunny_480p_20mb.mp4
+wget http://www.sample-videos.com/video/mp4/480/big_buck_bunny_480p_30mb.mp4
+wget http://www.sample-videos.com/video/mp4/360/big_buck_bunny_360p_1mb.mp4
+wget http://www.sample-videos.com/video/mp4/360/big_buck_bunny_360p_2mb.mp4
+wget http://www.sample-videos.com/video/mp4/360/big_buck_bunny_360p_5mb.mp4
+wget http://www.sample-videos.com/video/mp4/360/big_buck_bunny_360p_10mb.mp4
+wget http://www.sample-videos.com/video/mp4/360/big_buck_bunny_360p_20mb.mp4
+wget http://www.sample-videos.com/video/mp4/360/big_buck_bunny_360p_30mb.mp4
+wget http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_1mb.mp4
+wget http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_2mb.mp4
+wget http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_5mb.mp4
+wget http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_10mb.mp4
+wget http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_20mb.mp4
+wget http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_30mb.mp4
+
+wget http://www.sample-videos.com/video/flv/720/big_buck_bunny_720p_1mb.flv
+wget http://www.sample-videos.com/video/flv/720/big_buck_bunny_720p_2mb.flv
+wget http://www.sample-videos.com/video/flv/720/big_buck_bunny_720p_5mb.flv
+wget http://www.sample-videos.com/video/flv/720/big_buck_bunny_720p_10mb.flv
+
+wget http://www.sample-videos.com/video/mkv/720/big_buck_bunny_720p_1mb.mkv
+wget http://www.sample-videos.com/video/mkv/720/big_buck_bunny_720p_2mb.mkv
+wget http://www.sample-videos.com/video/mkv/720/big_buck_bunny_720p_5mb.mkv
+wget http://www.sample-videos.com/video/mkv/720/big_buck_bunny_720p_10mb.mkv
+
+
